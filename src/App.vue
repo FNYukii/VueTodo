76147bde9e18fe9cb6ef5f659bb70f2e22219747
FNYukii <forestgreen14689@gmail.com>
2025-04-12 15:17:09 +0900

    feat: TopScreen

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Header from './parts/Header.vue'
+import TopScreen from './screens/TopScreen.vue'
 </script>
 
 <template>
   <div class="container max-w-[1024px] mx-auto px-4 sm:px-0">
     <Header />
+    <TopScreen />
   </div>
 </template>
---
+++ src/screens/TopScreen.vue
@@ -0,0 +1,41 @@
+<script setup>
+const SAMPLE_TODOS = [
+  {
+    id: 'aaa',
+    content: '買い物',
+  },
+  {
+    id: 'bbb',
+    content: '洗濯',
+  },
+]
+</script>
+
+<template>
+  <main>
+    <div class="mt-8 flex justify-between items-center">
+      <h1 class="text-2xl font-bold">Todos</h1>
+
+      <a href="/new" class="outlined-button -my-2">新規Todo</a>
+    </div>
+
+    <div class="mt-8">
+      <p v-if="SAMPLE_TODOS.length === 0" class="text-secondary">
+        Todoがありません
+      </p>
+
+      <div
+        v-if="SAMPLE_TODOS.length !== 0"
+        class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8"
+      >
+        <a
+          v-for="todo in SAMPLE_TODOS"
+          v-bind:href="'/todos/' + todo.id"
+          class="p-4 border border-iron transition outline-none hover:bg-iron-pale focus:bg-iron-pale active:bg-iron-pale"
+        >
+          <p class="whitespace-pre">{{ todo.content }}</p>
+        </a>
+      </div>
+    </div>
+  </main>
+</template>
