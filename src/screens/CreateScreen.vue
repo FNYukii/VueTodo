09e78dce3f73dc8963c7fe0e30ca3244c464794d
FNYukii <forestgreen14689@gmail.com>
2025-04-12 16:23:26 +0900

    feat: CreateScreen

--- src/screens/CreateScreen.vue
+++ src/screens/CreateScreen.vue
@@ -1,5 +1,27 @@
+<script setup>
+import { ref } from 'vue'
+
+const content = ref('')
+</script>
+
 <template>
   <main>
-    <p class="mt-8">CreateScreen here</p>
+    <div className="mt-8">
+      <h1 className="text-2xl font-bold">Todoの作成</h1>
+    </div>
+
+    <textarea
+      v-model="content"
+      placeholder="新しいTodo"
+      class="mt-8 outlined-textarea"
+    />
+
+    <div className="mt-8   flex justify-between">
+      <RouterLink to="/" className="outlined-button"> 戻る </RouterLink>
+
+      <button v-bind:disabled="content.length === 0" className="filled-button">
+        作成
+      </button>
+    </div>
   </main>
 </template>
