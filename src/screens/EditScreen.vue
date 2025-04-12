311ea71c8dfe58ee7a133acba2d6d519752f7d79
FNYukii <forestgreen14689@gmail.com>
2025-04-12 16:36:56 +0900

    feat: EditScreen

--- src/screens/EditScreen.vue
+++ src/screens/EditScreen.vue
@@ -1,5 +1,37 @@
+<script setup>
+import { ref } from 'vue'
+import { generateSampleTodoContent } from '../utils/form'
+
+const content = ref('')
+</script>
+
 <template>
   <main>
-    <p class="mt-8">EditScreen here</p>
+    <div className="mt-8">
+      <h1 className="text-2xl font-bold">Todoの作成</h1>
+    </div>
+
+    <textarea
+      v-model="content"
+      v-bind:placeholder="generateSampleTodoContent()"
+      class="mt-8 outlined-textarea"
+    />
+
+    <div className="mt-8   flex justify-between items-start">
+      <RouterLink to="/" className="outlined-button"> 戻る </RouterLink>
+
+      <div className="flex gap-8   flex-wrap justify-end">
+        <button onClick="{handleDelete}" className="outlined-button">
+          削除
+        </button>
+
+        <button
+          v-bind:disabled="content.length === 0"
+          className="filled-button"
+        >
+          保存
+        </button>
+      </div>
+    </div>
   </main>
 </template>
