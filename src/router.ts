ceccb785ffaf89ae4db69025f7981e9748ec3aef
FNYukii <forestgreen14689@gmail.com>
2025-04-12 16:45:15 +0900

    feat: NotFoundScreen

--- src/router.ts
+++ src/router.ts
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 import TopScreen from './screens/TopScreen.vue'
 import CreateScreen from './screens/CreateScreen.vue'
 import EditScreen from './screens/EditScreen.vue'
+import NotFoundScreen from './screens/NotFoundScreen.vue'
 
 const routes = [
   { path: '/', component: TopScreen },
   { path: '/new', component: CreateScreen },
   { path: '/todos/:id', component: EditScreen },
+  { path: '/:notFound(.*)', component: NotFoundScreen },
 ]
 
 const router = createRouter({
---
+++ src/screens/NotFoundScreen.vue
@@ -0,0 +1,13 @@
+<template>
+  <main>
+    <div class="mt-8">
+      <h1 class="text-2xl font-bold">Page not Found</h1>
+    </div>
+
+    <p class="mt-8 text-secondary">ページが見つかりませんでした</p>
+
+    <div class="mt-8 flex justify-between">
+      <RouterLink to="/" class="outlined-button"> 戻る </RouterLink>
+    </div>
+  </main>
+</template>
