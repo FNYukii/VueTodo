81384699b4dddbf178850dc13aaf84877cf72cab
FNYukii <forestgreen14689@gmail.com>
2025-04-13 00:16:44 +0900

    feat: Todosを読み取り

--- src/screens/TopScreen.vue
+++ src/screens/TopScreen.vue
@@ -1,16 +1,9 @@
 <script setup>
+import { readTodos } from '../utils/storage'
+
 document.title = 'Vue Todo'
 
-const SAMPLE_TODOS = [
-  {
-    id: 'aaa',
-    content: '買い物',
-  },
-  {
-    id: 'bbb',
-    content: '洗濯',
-  },
-]
+const todos = readTodos()
 </script>
 
 <template>
@@ -22,16 +15,14 @@ const SAMPLE_TODOS = [
     </div>
 
     <div class="mt-8">
-      <p v-if="SAMPLE_TODOS.length === 0" class="text-secondary">
-        Todoがありません
-      </p>
+      <p v-if="todos.length === 0" class="text-secondary">Todoがありません</p>
 
       <div
-        v-if="SAMPLE_TODOS.length !== 0"
+        v-if="todos.length !== 0"
         class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8"
       >
         <RouterLink
-          v-for="todo in SAMPLE_TODOS"
+          v-for="todo in todos"
           v-bind:to="'/todos/' + todo.id"
           class="p-4 border border-iron transition outline-none hover:bg-iron-pale focus:bg-iron-pale active:bg-iron-pale"
         >
