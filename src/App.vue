e74a80dbd1128014583ffa3920ebfc16666def16
FNYukii <forestgreen14689@gmail.com>
2025-04-13 14:35:34 +0900

    feat: containerにpb設定

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import TopScreen from './screens/TopScreen.vue'
 </script>
 
 <template>
-  <div class="container max-w-[1024px] mx-auto px-4">
+  <div class="container max-w-[1024px] mx-auto px-4 pb-8">
     <Header />
     <RouterView />
   </div>
