97eec756377f951bc4340272a82f1ef9591e477a
FNYukii <forestgreen14689@gmail.com>
2025-04-15 21:51:27 +0900

    clean: コード簡略化

--- src/parts/Header.vue
+++ src/parts/Header.vue
@@ -2,7 +2,7 @@
   <header class="mt-4">
     <RouterLink
       to="/"
-      class="text-4xl font-light text-accent py-1 px-4 -my-1 -mx-4 border border-transparent outline-none focus-visible:border focus-visible:border-accent focus-visible:bg-button-hover"
+      class="text-4xl font-light text-accent py-1 px-4 -my-1 -mx-4 focus-visible:outline-solid focus-visible:outline-accent"
     >
       Vue Todo
     </RouterLink>
